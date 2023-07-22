2a591e3d961af1d01300d4f984a7ea650f88c834
Hector <baseballhector@gmail.com>
2023-07-22 18:24:27 -0400

    Append function

--- scripts/linkedList.js
+++ scripts/linkedList.js
@@ -4,7 +4,27 @@ import { node } from "./node.js";
 const linkedList = () => {
   var root = node();
 
-  return { root, tail };
+  // Appends a node to the end of the list.
+  const append = (value) => {
+    // Create new node.
+    const newNode = node(value);
+
+    // If no nodes in list, newNode assign to root's next/
+    if (root.next === null) {
+      root.next = newNode;
+      return;
+    }
+
+    // Goes to the end of the list.
+    while (root.next !== null) {
+      root = root.next;
+    }
+
+    // Assigns new node.
+    root.next = newNode;
+  };
+
+  return { root, append };
 };
 
 export { linkedList };
